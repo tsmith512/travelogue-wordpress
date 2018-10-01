3708078e6302e81552a735f6d33d8382bf023a90
Taylor Smith <tsmith@tsmithcreative.com>
2018-10-01 17:26:07 +0000

    Install Colorbox and use it to open image links

--- wp-content/themes/travelogue-theme/functions.php
+++ wp-content/themes/travelogue-theme/functions.php
@@ -6,5 +6,24 @@
 function travelogue_theme_enqueue_parent_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'travelogue_theme_enqueue_parent_styles' );
+add_action( 'wp_enqueue_scripts', 'travelogue_theme_enqueue_parent_styles', 5 );
 
+/**
+ * Implements wp_enqueue_scripts to enqueue scripts/styles for my overrides
+ */
+function travelogue_theme_enqueue_own_scripts_and_styles() {
+  wp_enqueue_script('travelogue-js-media', get_stylesheet_directory_uri() . '/js/media.js', array('colorbox-script', 'jquery'), false, true);
+}
+add_action( 'wp_enqueue_scripts', 'travelogue_theme_enqueue_own_scripts_and_styles', 20 );
+
+/**
+ * Implements wp_enqueue_scripts to enqueue the scripts and styles for the
+ * Colorbox lightbox library which will attach to all image galleries.
+ *
+ * @TODO: Can this be executed only when needed? Also replace with PhotoSwipe
+ */
+function travelogue_theme_enqueue_lightbox() {
+  wp_enqueue_style( 'colorbox-style', get_stylesheet_directory_uri() . '/vendor/colorbox/example3/colorbox.css', array());
+  wp_enqueue_script('colorbox-script', get_stylesheet_directory_uri() . '/vendor/colorbox/jquery.colorbox.js', array('jquery'), false, true);
+}
+add_action( 'wp_enqueue_scripts', 'travelogue_theme_enqueue_lightbox', 10 );
---
+++ wp-content/themes/travelogue-theme/js/media.js
@@ -0,0 +1,12 @@
+(function($){
+  'use strict';
+
+  $(document).ready(function(){
+    $('.gallery-item a').colorbox({
+      maxWidth: "95%",
+      maxHeight: "95%"
+    });
+  });
+
+  console.log('howdy');
+})(jQuery);
