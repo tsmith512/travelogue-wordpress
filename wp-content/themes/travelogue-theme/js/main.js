3cb1de30c5e5f8e742bb3afa52c32d21acf38b58
Taylor Smith <tsmith@tsmithcreative.com>
2018-10-02 06:26:14 +0000

    Add in theme's main js starter file

---
+++ wp-content/themes/travelogue-theme/js/main.js
@@ -0,0 +1,8 @@
+(function(){
+  var sidebar = new StickySidebar('#secondary', {
+    containerSelector: '.site-content > .wrap',
+    innerWrapperSelector: '.secondary_inner',
+    topSpacing: 40,
+    bottomSpacing: 40
+  });
+})();
