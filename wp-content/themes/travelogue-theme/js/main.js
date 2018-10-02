e0741890a05ca9826689d6749201576d0c7811c5
Taylor Smith <tsmith@tsmithcreative.com>
2018-10-02 06:46:58 +0000

    And don't enable it on 1-col layouts

--- wp-content/themes/travelogue-theme/js/main.js
+++ wp-content/themes/travelogue-theme/js/main.js
@@ -4,7 +4,8 @@
       containerSelector: '.site-content > .wrap',
       innerWrapperSelector: '.secondary_inner',
       topSpacing: 40,
-      bottomSpacing: 40
+      bottomSpacing: 40,
+      minWidth: 768 /* Twentyseventeen's CSS is 48em */
     });
   });
 })();
