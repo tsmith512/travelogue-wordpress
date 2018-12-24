47277da9d6f4d6877e60f653d9e465e39ef1eb3f
Taylor Smith <tsmith@tsmithcreative.com>
2018-12-24 21:21:53 +0000

    Show more numbers in the paginator

--- wp-content/themes/travelogue-theme/index.php
+++ wp-content/themes/travelogue-theme/index.php
@@ -43,6 +43,7 @@ get_header(); ?>
 				endwhile;
 
 				the_posts_pagination( array(
+					'mid_size' => 3,
 					'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
 					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
 					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
