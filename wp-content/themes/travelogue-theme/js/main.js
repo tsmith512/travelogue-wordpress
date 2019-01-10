1bc0186528442d3499c163a98995167caf689fab
Taylor Smith <tsmith@tsmithcreative.com>
2019-01-10 20:17:10 +0000

    Add a floating quote icon outside blockquotes in standard posts like a format-quote post

--- wp-content/themes/travelogue-theme/js/main.js
+++ wp-content/themes/travelogue-theme/js/main.js
@@ -7,5 +7,8 @@
       bottomSpacing: 40,
       minWidth: 768 /* Twentyseventeen's CSS is 48em */
     });
+
+    // Set icon for blockquotes used in regular posts, like quote-format posts
+    jQuery( twentyseventeenScreenReaderText.quote ).prependTo('article.format-standard blockquote');
   });
 })();
