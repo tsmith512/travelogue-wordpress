1ff0fa57d7c8ea167bbddc69bcac04a140e26b77
Taylor Smith <tsmith@tsmithcreative.com>
2019-01-21 06:03:03 +0000

    #11 re 0e9f3ba: Open quote icon was also in there, replace with simple text quote from the active typeface. Ref #10.

--- wp-content/themes/rnf-alfa/js/main.js
+++ wp-content/themes/rnf-alfa/js/main.js
@@ -7,8 +7,5 @@
       bottomSpacing: 40,
       minWidth: 768 /* Twentyseventeen's CSS is 48em */
     });
-
-    // Set icon for blockquotes used in regular posts, like quote-format posts
-    jQuery( twentyseventeenScreenReaderText.quote ).prependTo('article.format-standard blockquote');
   });
 })();
