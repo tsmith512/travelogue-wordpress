d6c06f41612d9e5f6f91797dcfcd6feaaa1a19f8
Taylor Smith <tsmith@tsmithcreative.com>
2019-01-21 06:17:25 +0000

    #11: Move the SVG support detection script into RNF Alfa because its script was dropped from Twentyseventeen

--- wp-content/themes/rnf-alfa/js/main.js
+++ wp-content/themes/rnf-alfa/js/main.js
@@ -1,4 +1,14 @@
 (function(){
+  /*
+   * Test if inline SVGs are supported.
+   * @link https://github.com/Modernizr/Modernizr/
+   */
+  function supportsInlineSVG() {
+    var div = document.createElement( 'div' );
+    div.innerHTML = '<svg/>';
+    return 'http://www.w3.org/2000/svg' === ( 'undefined' !== typeof SVGRect && div.firstChild && div.firstChild.namespaceURI );
+  }
+
   document.addEventListener("DOMContentLoaded", function() {
     var sidebar = new StickySidebar('#secondary', {
       containerSelector: '.site-content > .wrap',
@@ -7,5 +17,10 @@
       bottomSpacing: 40,
       minWidth: 768 /* Twentyseventeen's CSS is 48em */
     });
+
+    if ( true === supportsInlineSVG() ) {
+      document.documentElement.className = document.documentElement.className.replace( /(\s*)no-svg(\s*)/, '$1svg$2' );
+    }
   });
+
 })();
