3b8b95386358b9f93ee9110819017da1bc061418
Taylor Smith <tsmith@tsmithcreative.com>
2019-02-02 04:40:00 +0000

    #11: Update the archive pagination links to not use the icon

--- wp-content/themes/rnf-alfa/archive.php
+++ wp-content/themes/rnf-alfa/archive.php
@@ -43,13 +43,12 @@ get_header(); ?>
 
       endwhile;
 
-      the_posts_pagination(
-        array(
-          'prev_text'          => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-          'next_text'          => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-          'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-        )
-      );
+      the_posts_pagination( array(
+        'mid_size' => 3,
+        'prev_text' => '&blacktriangleleft;<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+        'next_text' => '&blacktriangleright;<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>',
+        'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+      ) );
 
     else :
 
